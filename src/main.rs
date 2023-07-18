697e5b096d01bc2a73660137c08232959d5bf790
Gaurav <gaurav.mahto2@aristocrat.com>
2023-07-18 17:47:05 +0530

    Random

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
+use rand::Rng;
 use std::{thread, time};
 
-use mouse_rs::{types::keys::Keys, Mouse};
+// use mouse_rs::{types::keys::Keys, Mouse};
+use mouse_rs::Mouse;
 
-fn move_and_press() {
+fn move_and_press(last_mouse_pos: (i32, i32)) -> (i32, i32) {
     let mouse = Mouse::new();
-    // mouse.move_to(500, 500).expect("Unable to move mouse");
-    mouse.press(&Keys::RIGHT).expect("Unable to press button");
+    let mut mouse_pos = mouse.get_position().unwrap();
+
+    if (mouse_pos.x != last_mouse_pos.0) && (mouse_pos.y != last_mouse_pos.1) {
+        return (mouse_pos.x, mouse_pos.y);
+    }
+
+    let mut rng = rand::thread_rng();
+
+    let n1 = rng.gen_range(0..1024);
+    let n2 = rng.gen_range(0..1024);
+
+    mouse.move_to(n1, n2).expect("Unable to move mouse");
+    // mouse.press(&Keys::RIGHT).expect("Unable to press button");
     // mouse
     //     .release(&Keys::RIGHT)
     //     .expect("Unable to release button");
+
+    mouse_pos = mouse.get_position().unwrap();
+
+    return (mouse_pos.x, mouse_pos.y);
 }
 
 fn main() {
@@ -16,8 +33,10 @@ fn main() {
 
     let ten_secs = time::Duration::from_secs(10);
 
+    let mut last_mouse_pos: (i32, i32) = (0, 0);
+
     loop {
-        move_and_press();
+        last_mouse_pos = move_and_press(last_mouse_pos);
         thread::sleep(ten_secs);
     }
 }
